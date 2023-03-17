701a16514bd02975c526ac8016e944528437edaf
Eduardo Cardoso <educardosorv.carmo@gmail.com>
2023-03-17 20:26:24 -0300

    requisito 08

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,12 +3,18 @@ import PropTypes from 'prop-types';
 
 class ProductCard extends Component {
   render() {
-    const { title, thumbnail, price } = this.props;
+    const { title, thumbnail, price, getProduct } = this.props;
     return (
       <div data-testid="product">
         <p>{ title }</p>
         <img src={ thumbnail } alt="" />
         <p>{ price }</p>
+        <button
+          data-testid="product-add-to-cart"
+          onClick={ () => getProduct(title, thumbnail, price) }
+        >
+          Comprar
+        </button>
       </div>
     );
   }
@@ -18,5 +24,6 @@ ProductCard.propTypes = {
   title: PropTypes.string.isRequired,
   thumbnail: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
+  getProduct: PropTypes.func.isRequired,
 };
 export default ProductCard;
