7eae1bb4b8108aec8dd1778d76fd6b7d5f585938
Kevin-Amaral165 <kevin_ramos1@hotmail.com>
2023-03-17 16:31:41 -0300

    requisito04

--- src/components/ListProducts.jsx
+++ src/components/ListProducts.jsx
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
 export default class ListProducts extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    getCategories().then((categories) => {
+      this.setState({ categories });
+    });
+  }
+
   render() {
+    const { categories } = this.state;
+
     return (
       <div>
         <input type="text" />
-        <p
-          data-testid="home-initial-message"
-        >
+        <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <div>
+          {categories.map(({ id, name }) => (
+            <button key={ id } data-testid="category">
+              {name}
+            </button>
+          ))}
+        </div>
       </div>
     );
   }
